74b2e7d4a240a7eb916233b743513a94ed213779
ghfrty <bro_coly@hotmail.fr>
2015-04-13 14:48:26 +0200

    Avancement Exercice 1

---
+++ ACPM/Graphe.h
@@ -0,0 +1,19 @@
+#ifndef GRAPHE_H
+#define GRAPHE_H
+
+#include <vector>
+#include <iostream>
+
+class Graphe
+{
+private:
+    int* sommets;
+    vector<int *> aretes;
+    
+public:
+    Graphe();
+    
+};
+
+
+#endif
